688bdb3982055ec01cb8d1815c933a1c6e8ae3bb
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-04-28 18:59:01 +0200

    Se han añadido las primary y foreign key

--- DataBase.sql
+++ DataBase.sql
@@ -6,22 +6,31 @@ use Apuntes;
     --    AÑO    --
 create table ANIO (
     idAnio    int(11)   not null,
-    anio      char(9)   default(null)
+    anio      char(9)   default(null),
+
+		primary key (idAnio)
 );
 
 
     --   DOCUMENTOS    --
-create table DOCUMENTO (
-    idApuntes     int(11)       not null,
+create table DOCUMENTOS (
+    idDocumento   int(11)       not null,
     titulo        varchar(50)   not null,
-    usuario       int(11)       not null,
+    usuario       int(11)       default null,
     fechaSubida   datetime      default current_timestamp,
     tipo          int(11)       not null,
     anio          int(11)       not null,
     documento     varchar(50)   not null,
     hash          varchar(32)   not null,
     comentario    varchar(300)  not null,
-    asignatura    int(11)       not null
+    asignatura    int(11)       not null,
+
+		primary key   (idApuntes),
+		foreign key   (usuario)     references  USUARIO(idUsuario),
+		foreign key   (tipo)        references  TIPO(idTipo),
+		foreign key   (anio)        references  ANIO(idAnio),
+		foreign key   (asignatura)  references  ASIGNATURA(idAsignatura)
+
 );
 
 
@@ -31,13 +40,10 @@ create table ASIGNATURA (
     codigo          varchar(10)     not null,
     nombre          varchar(100)    not null,
     estudios        int(11)         not null,
-    curso           int(11)         not null
-);
+    curso           int(1)         not null,
 
-
-    --   CURSO    --
-create table CURSO (
-    idCurso	int(11)	not null
+		primary key    (idAsigantura),
+		foreign key    (estudios)        references ESTUDIOS(idEstudios) 
 );
 
 
@@ -45,12 +51,16 @@ create table CURSO (
 create table COMENTARIOS (
     idComentario    int(11),
     idUsuario       int(11),
-    idApuntes       int(11),
+    idDocumento     int(11),
     puntuacion      float,
     comentario      varchar(300),
     fecha           date,
     Pseudonimo      char(12),
-    constraint pk_coment primary key(idComentarios),
+
+    primary key    (idComentarios),
+		foreign key    (idUsuario)      references  USUARIO(idUsuario),
+    foreign key    (idDocumento)    references  DOCUMENTOS(idDocumento),
+
     unique key usr_apunt (idUsuario, idApuntes),
     unique key apunt_pseu (idApunte, pseudonimo)
 );
@@ -58,22 +68,29 @@ create table COMENTARIOS (
 
     --   ESTUDIOS    --
 create table ESTUDIOS (
-    idEstudios    int(11),
-    nombre        varchar(12)
+    idEstudios    int(11)        not null,
+    nombre        varchar(12)    not null,
+
+		primary key (idEstudios)
 );
 
 
     --   SESION    --
 create table SESION (
     idSesion    int(11),
-    idUsuario   int(11)
+    idUsuario   int(11),
+
+		primary key    (idSesion),
+		foreign key    (idUsuario)  references  USUARIO(idUsuario)
 );
 
 
     --   TIPO    --
 create table TIPO (
     idTipo    int(11),
-    nombre    varchar(12)
+    nombre    varchar(12),
+
+		primary key (idTipo)
 );
 
 
@@ -85,7 +102,9 @@ create table USUARIO (
     apellido2       varchar(30)     default null,
     password        varchar(34),
     nick            varchar(20),
-    email           varchar(50)
+    email           varchar(50),
+
+		primary key (idUsuario)
 );
 
 
@@ -93,4 +112,6 @@ create table USUARIO (
 create table INVITACION (
     idCodigo    int(11),
     codigo      char(12),
+
+		primary key (idCodigo)
 );
