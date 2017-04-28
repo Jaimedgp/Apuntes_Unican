02aa5c38f8f821aa5973ea04c58219ff81cf5a57
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-04-28 13:58:35 +0200

    Hacerlo mas bonito

--- DataBase.sql
+++ DataBase.sql
@@ -3,94 +3,94 @@ create database if not exists Apuntes;
 use Apuntes;
 
 
-	--	AÑO	--
+    --    AÑ    --
 create table ANIO (
-	idAnio		int(11)		not null,
-	anio			char(9)		default(null)
+    idAnio    int(11)   not null,
+    anio      char(9)   default(null)
 );
 
 
-	--	DOCUMENTOS	-- 
+    --   DOCUMENTOS    --
 create table DOCUMENTO (
-	idApuntes			int(11)				not null,
-	titulo				varchar(50)		not null,
-	usuario				int(11)				not null,
-	fechaSubida		datetime			default current_timestamp,
-	tipo					int(11)				not null,
-	anio					int(11)				not null,
-	documento			varchar(50)		not null,
-	hash					varchar(32)		not null,
-	comentario		varchar(300)	not null,
-	asignatura		int(11)				not null
+    idApuntes     int(11)       not null,
+    titulo        varchar(50)   not null,
+    usuario       int(11)       not null,
+    fechaSubida   datetime      default current_timestamp,
+    tipo          int(11)       not null,
+    anio          int(11)       not null,
+    documento     varchar(50)   not null,
+    hash          varchar(32)   not null,
+    comentario    varchar(300)  not null,
+    asignatura    int(11)       not null
 );
 
 
-	--	ASIGNATURA	--
+    --   ASIGNATURA    --
 create table ASIGNATURA (
-	idAsignatura	int(11)				not null,
-	codigo				varchar(10)		not null,
-	nombre				varchar(100)	not null,
-	estudios			int(11)				not null,
-	curso					int(11)				not null
+    idAsignatura    int(11)         not null,
+    codigo          varchar(10)     not null,
+    nombre          varchar(100)    not null,
+    estudios        int(11)         not null,
+    curso           int(11)         not null
 );
 
 
-	--	CURSO	--
+    --   CURSO    --
 create table CURSO (
-	idCurso	int(11)	not null
+    idCurso	int(11)	not null
 );
 
 
-	--	COMENTARIOS	--
+    --   COMENTARIOS    --
 create table COMENTARIOS (
-	idComentario		int(11),
-	idUsuario				int(11),
-	idApuntes				int(11),
-	puntuacion			float,
-	comentario			varchar(300),
-	fecha						date,
-	Pseudonimo			char(12),
-	constraint pk_coment primary key(idComentarios),
-	unique key usr_apunt (idUsuario, idApuntes),
-	unique key apunt_pseu (idApunte, pseudonimo)
+    idComentario    int(11),
+    idUsuario       int(11),
+    idApuntes       int(11),
+    puntuacion      float,
+    comentario      varchar(300),
+    fecha           date,
+    Pseudonimo      char(12),
+    constraint pk_coment primary key(idComentarios),
+    unique key usr_apunt (idUsuario, idApuntes),
+    unique key apunt_pseu (idApunte, pseudonimo)
 );
 
 
-	--	ESTUDIOS	--
+    --   ESTUDIOS    --
 create table ESTUDIOS (
-	idEstudios	int(11),
-	nombre			varchar(12)
+    idEstudios    int(11),
+    nombre        varchar(12)
 );
 
 
-	-- SESION	--
+    --   SESION    --
 create table SESION (
-	idSesion		int(11),
-	idUsuario		int(11)
+    idSesion    int(11),
+    idUsuario   int(11)
 );
 
 
-	--	TIPO	--
+    --   TIPO    --
 create table TIPO (
-	idTipo	int(11),
-	nombre	varchar(12)
+    idTipo    int(11),
+    nombre    varchar(12)
 );
 
 
-	--	USUARIO	--
+    --   USUARIO    --
 create table USUARIO (
-	idUsuario		int(11),
-	nombre			varchar(30),
-	apellido1		varchar(30),
-	apellido2		varchar(30)		default null,
-	password		varchar(34),
-	nick				varchar(20),
-	email				varchar(50)
+    idUsuario       int(11),
+    nombre          varchar(30),
+    apellido1       varchar(30),
+    apellido2       varchar(30)     default null,
+    password        varchar(34),
+    nick            varchar(20),
+    email           varchar(50)
 );
 
 
-	-- INVITACION	--
+    --   INVITACION    --
 create table INVITACION (
-	idCodigo	int(11),
-	codigo		char(12),
+    idCodigo    int(11),
+    codigo      char(12),
 );
