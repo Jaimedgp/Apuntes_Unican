e230145dcecaa0c56d79060333b121eebfb82921
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-05-01 20:43:43 +0200

    Primera version estable

--- DataBase.sql
+++ DataBase.sql
@@ -7,34 +7,35 @@ use Apuntes;
 create table ANIO (
     idAnio    int(11)   not null,
     anio      char(9),
-
-    primary key (idAnio),
-
-    unique (anio)
+    unique (anio),
+    primary key (idAnio)
 );
 
 
-    --   DOCUMENTOS    --
-create table DOCUMENTOS (
-    idDocumento   int(11)       not null,
-    titulo        varchar(50)   not null,
-    usuario       int(11),
-    fechaSubida   datetime      default current_timestamp,
-    tipo          int(11)       not null,
-    anio          int(11)       not null,
-    documento     varchar(50)   not null,
-    hash          varchar(32)   not null,
-    comentario    varchar(300)  not null,
-    asignatura    int(11)       not null,
+    --   ESTUDIOS    --
+create table ESTUDIOS (
+    idEstudios    int(11)        not null,
+    nombre        varchar(12)    not null,
 
-    primary key   (idApuntes),
-    foreign key   (usuario)     references  USUARIO(idUsuario),
-    foreign key   (tipo)        references  TIPO(idTipo),
-    foreign key   (anio)        references  ANIO(idAnio),
-    foreign key   (asignatura)  references  ASIGNATURA(idAsignatura),
+    primary key (idEstudios)
+);
 
-    unique (hash)
 
+    --   TIPO    --
+create table TIPO (
+    idTipo    int(11)        not null,
+    nombre    varchar(12)    not null,
+
+    primary key (idTipo)
+);
+
+
+    --   INVITACION    --
+create table INVITACION (
+    idCodigo    int(11),
+    codigo      char(12)    not null,
+
+    primary key (idCodigo)
 );
 
 
@@ -46,38 +47,30 @@ create table ASIGNATURA (
     estudios        int(11)         not null,
     curso           int(1)          not null,
 
-    primary key    (idAsigantura),
+    primary key    (idAsignatura),
     foreign key    (estudios)        references ESTUDIOS(idEstudios),
 
     unique (codigo)
 );
 
 
-    --   COMENTARIOS    --
-create table COMENTARIOS (
-    idComentario    int(11),
+    --   USUARIO    --
+create table USUARIO (
     idUsuario       int(11),
-    idDocumento     int(11),
-    puntuacion      float,
-    comentario      varchar(300),
-    fecha           date,
-    Pseudonimo      char(12),
+    nombre          varchar(30)    not null,
+    apellido1       varchar(30)    not null,
+    apellido2       varchar(30),
+    password        varchar(34)    not null,
+    nick            varchar(20)    not null,
+    email           varchar(50)    not null,
+    puntuación      float,
+    estudios        int(11),
 
-    primary key    (idComentarios),
-    foreign key    (idUsuario)      references  USUARIO(idUsuario),
-    foreign key    (idDocumento)    references  DOCUMENTOS(idDocumento),
+    unique (nick),
+    unique (nombre, apellido1, apellido2),
 
-    constraint usr_apunt  unique (idUsuario, idApuntes ),
-    constraint apunt_pseu unique (idApunte , pseudonimo)
-);
-
-
-    --   ESTUDIOS    --
-create table ESTUDIOS (
-    idEstudios    int(11)        not null,
-    nombre        varchar(12)    not null,
-
-    primary key (idEstudios)
+    primary key    (idUsuario),
+    foreign key    (estudios)   references  ESTUDIOS(idEstudios)
 );
 
 
@@ -91,36 +84,46 @@ create table SESION (
 );
 
 
-    --   TIPO    --
-create table TIPO (
-    idTipo    int(11)        not null,
-    nombre    varchar(12)    not null,
+    --   DOCUMENTOS    --
+create table DOCUMENTOS (
+    idDocumento   int(11)       not null,
+    titulo        varchar(50)   not null,
+    usuario       int(11),
+    fechaSubida   datetime      default current_timestamp,
+    tipo          int(11)       not null,
+    anio          int(11)       not null,
+    documento     varchar(50)   not null,
+    hash          varchar(32)   not null,
+    asignatura    int(11)       not null,
+    comentario    varchar(300),
+    puntuacion    float,
 
-    primary key (idTipo)
+    unique (hash),
+
+    primary key   (idDocumento),
+    foreign key   (usuario)     references  USUARIO(idUsuario),
+    foreign key   (tipo)        references  TIPO(idTipo),
+    foreign key   (anio)        references  ANIO(idAnio),
+    foreign key   (asignatura)  references  ASIGNATURA(idAsignatura)
 );
 
 
-    --   USUARIO    --
-create table USUARIO (
+    --   COMENTARIOS    --
+create table COMENTARIOS (
+    idComentario    int(11),
     idUsuario       int(11),
-    nombre          varchar(30)    not null,
-    apellido1       varchar(30)    not null,
-    apellido2       varchar(30),
-    password        varchar(34)    not null,
-    nick            varchar(20)    not null,
-    email           varchar(50)    not null,
+    idDocumento     int(11),
+    puntuacion      float,
+    comentario      varchar(300),
+    fecha           date,
+    pseudonimo      char(12),
 
-    primary key (idUsuario),
+    primary key    (idComentario),
+    foreign key    (idUsuario)      references  USUARIO(idUsuario),
+    foreign key    (idDocumento)    references  DOCUMENTOS(idDocumento),
 
-		unique (nick),
-		unique (nombre, apellido1, apellido2)
-);
+    constraint usr_apunt  unique (idUsuario, idDocumento),
+    constraint apunt_pseu unique (idDocumento , pseudonimo),
 
-
-    --   INVITACION    --
-create table INVITACION (
-    idCodigo    int(11),
-    codigo      char(12)    not null,
-
-    primary key (idCodigo)
+    check (puntuacion < 5.0)
 );
--- Valores.sql
+++ Valores.sql
@@ -2,55 +2,55 @@ use Apuntes;
 
 insert into ANIO (idAnio ,Anio       )
           values (0      ,'2010/2011'),
-          values (1      ,'2011/2012'),
-          values (2      ,'2012/2013'),
-          values (3      ,'2013/2014'),
-          values (4      ,'2014/2015'),
-          values (5      ,'2015/2016'),
-          values (6      ,'2016/2017')
-;
-
-insert into DOCUMENTOS (idApuntes, titulo                           , usuario, fechaSubida        , tipo, anio, documento            , hash                 , comentario                       , asignatura)
-                values (2        , 'Examen Parcial Inexistente'     , 1      , 2017-04-05 14:59:09, 1   , 3   , 'examenparcial.pdf'  , '832rn0xyrqx8yfn08'  , 'Examen parcial del año la polka', 2         ),
-                values (3        , 'No es lo mismo montar un follón', 1      , 2017-04-06 23:42:54, 0   , 3   , 'noes.pdf'           , ' '                  , 'Libro sobre cosas'              , 3         ),
-                values (4        , 'Electromagnetismo inutil'       , 1      , 2017-04-06 23:43:27, 0   , 3   , 'no2es.pdf'          , ' '                  , 'Tengo cien de estos'            , 2         ),
-                values (80       , 'Electromagnetismo jodido'       , 2      , 2017-04-15 17:20:20, 0   , 5   , 'documentoFalso.pdf' , '238nx073rnw7'       , 'Electromagnetismo del jodido 
-                                                                                                                                                                 jodido, pero jodido de verdad', 2         )
-;
-
-insert into ASIGNATURA (idAsignatura, codigo  , nombre                                                       , estudios, curso)
-                values (1           , 'G53'   , 'Termodinámica'                                              , 0       , 2    ),
-                values (2           , 'G51'   , 'Electricidad y magnetismo'                                  , 0       , 2    ),
-                values (3           , 'G55'   , 'Física Cuántica y Estructura de la Materia I: Fundamentales', 0       , 2    ),
-                values (4           , 'G1722' , 'Habilidades, Valores y Competencias Transversales'          , 0       , 2    ),
-                values (5           , 'G62'   , 'Laboratorio de Física I'                                    , 0       , 2    ),
-                values (6           , 'G63'   , 'Laboratorio de Física II'                                   , 0       , 2    ),
-                values (7           , 'G49'   , 'Mecánica Clásica y Relatividad'                             , 0       , 2    ),
-                values (8           , 'G59'   , 'Ecuaciones Diferenciales Ordinarias'                        , 0       , 2    ),
-                values (9           , 'G60'   , 'Ecuaciones Derivadas Parciales'                             , 0       , 2    ),
-                values (10          , 'G261'  , 'Inglés'                                                     , 0       , 2    )
+                 (1      ,'2011/2012'),
+                 (2      ,'2012/2013'),
+                 (3      ,'2013/2014'),
+                 (4      ,'2014/2015'),
+                 (5      ,'2015/2016'),
+                 (6      ,'2016/2017')
 ;
 
 insert into ESTUDIOS (idEstudios, nombre                  )
               values (0         , 'Fisica'                ),
-              values (1         , 'Matematicas'           ),
-              values (2         , 'Ingenieria Informatica')
-;
-
-insert into SESION (idSesion                          , idUsuario)
-            values ('0a5ff911d3af9823ba2d50d77b6a6595', 1        ),
-            values ('8f7a2b3dddcf0ad39a4875f62a97907f', 8        )
+                     (1         , 'Matematicas'           ),
+                     (2         , 'Ingenieria Informatica')
 ;
 
 insert into TIPO (idTipo, nombre          )
           values (0     , 'Apuntes'       ),
-          values (1     , 'Examen Parcial'),
-          values (2     , 'Examen Final'  )
+                 (1     , 'Examen Parcial'),
+                 (2     , 'Examen Final'  )
+;
+
+insert into ASIGNATURA (idAsignatura, codigo  , nombre                                                       , estudios, curso)
+                values (1           , 'G53'   , 'Termodinámica'                                              , 0       , 2    ),
+                       (2           , 'G51'   , 'Electricidad y magnetismo'                                  , 0       , 2    ),
+                       (3           , 'G55'   , 'Física Cuántica y Estructura de la Materia I: Fundamentales', 0       , 2    ),
+                       (4           , 'G1722' , 'Habilidades, Valores y Competencias Transversales'          , 0       , 2    ),
+                       (5           , 'G62'   , 'Laboratorio de Física I'                                    , 0       , 2    ),
+                       (6           , 'G63'   , 'Laboratorio de Física II'                                   , 0       , 2    ),
+                       (7           , 'G49'   , 'Mecánica Clásica y Relatividad'                             , 0       , 2    ),
+                       (8           , 'G59'   , 'Ecuaciones Diferenciales Ordinarias'                        , 0       , 2    ),
+                       (9           , 'G60'   , 'Ecuaciones Derivadas Parciales'                             , 0       , 2    ),
+                       (10          , 'G261'  , 'Inglés'                                                     , 0       , 2    )
 ;
 
 insert into USUARIO (idUsuario, nombre , apellido1 , apellido2       , password                          , nick       , email                       )
              values (1        , 'David', 'Iglesias', 'Sánchez'       , '451d8bb9214d5cf16d1f22e5cbeb0ad2', 'Pelu'     , 'davidiglesanchez@gmail.com'),
-             values (2        , 'Jaime', 'Díez'    , 'González-Pardo', '81dc9bdb52d04dc20036dbd8313ed055', 'Jaimedgp' , 'davidiglesanchez@gmail.com'),
-             values (7        , 'David', 'Iglesias', 'Sánchez'       , '451d8bb9214d5cf16d1f22e5cbeb0ad2', 'davidigle', 'davidiglesanchez@gmail.com'),
-             values (8        , 'David', 'Iglesias', 'Sánchez'       , '451d8bb9214d5cf16d1f22e5cbeb0ad2', 'david'    , 'davidiglesanchez@gmail.com')
+                    (2        , 'Jaime', 'Díez'    , 'González-Pardo', '81dc9bdb52d04dc20036dbd8313ed055', 'Jaimedgp' , 'davidiglesanchez@gmail.com'),
+                    (7        , 'David', 'Iglesias', 'Sonchez'       , '451d8bb9214d5cf16d1f22e5cbeb0ad2', 'davidigle', 'davidiglesanchez@gmail.com'),
+                    (8        , 'David', 'Iglesias', 'Sánchiz'       , '451d8bb9214d5cf16d1f22e5cbeb0ad2', 'david'    , 'davidiglesanchez@gmail.com')
+;
+
+insert into SESION (idSesion                          , idUsuario)
+            values ('0a5ff911d3af9823ba2d50d77b6a6595', 1        ),
+                   ('8f7a2b3dddcf0ad39a4875f62a97907f', 8        )
+;
+
+insert into DOCUMENTOS (idDocumento, titulo                           , usuario, fechaSubida          , tipo, anio, documento            , hash                 , comentario                       , asignatura)
+                values (2          , 'Examen Parcial Inexistente'     , 1      , '2017-04-05 14:59:09', 1   , 3   , 'examenparcial.pdf'  , '832rn0xyrqx8yfn08'  , 'Examen parcial del año la polka', 2         ),
+                       (3          , 'No es lo mismo montar un follón', 1      , '2017-04-06 23:42:54', 0   , 3   , 'noes.pdf'           , '832rn0xyrqx8yfn09'  , 'Libro sobre cosas'              , 3         ),
+                       (4          , 'Electromagnetismo inutil'       , 1      , '2017-04-06 23:43:27', 0   , 3   , 'no2es.pdf'          , '238nx073rnw8'       , 'Tengo cien de estos'            , 2         ),
+                       (80         , 'Electromagnetismo jodido'       , 2      , '2017-04-15 17:20:20', 0   , 5   , 'documentoFalso.pdf' , '238nx073rnw7'       , 'Electromagnetismo del jodido 
+                                                                                                                                                                   jodido, pero jodido de verdad'  , 2         )
 ;
