049334d926e6ef5e47bdb7e7b065fdb390615f06
David Iglesias Sánchez <davidiglesanchez@gmail.com>
2017-05-04 19:22:03 +0200

    Databse corrected and adapted...
    Databse corrected and adapted for the PHP Script. First version

--- DataBase.sql
+++ DataBase.sql
@@ -1,54 +1,55 @@
-create database if not exists Apuntes;
+drop database if exists Apuntes;
+create database Apuntes;
 
 use Apuntes;
 
 
     --    AÑO    --
 create table ANIO (
-    idAnio    int(11)   not null,
-    anio      char(9),
-    unique (anio),
-    primary key (idAnio)
+    IdAnio    int(11)   not null,
+    Anio      char(9),
+    unique (Anio),
+    primary key (IdAnio)
 );
 
 
     --   ESTUDIOS    --
 create table ESTUDIOS (
-    idEstudios    int(11)        not null,
-    nombre        varchar(12)    not null,
+    IdEstudios    int(11)        not null,
+    Nombre        varchar(50)    not null,
 
-    primary key (idEstudios)
+    primary key (IdEstudios)
 );
 
 
     --   TIPO    --
 create table TIPO (
-    idTipo    int(11)        not null,
-    nombre    varchar(12)    not null,
+    IdTipo    int(11)        not null,
+    Nombre    varchar(25)    not null,
 
-    primary key (idTipo)
+    primary key (IdTipo)
 );
 
 
     --   INVITACION    --
 create table INVITACION (
-    idCodigo    int(11),
-    codigo      char(12)    not null,
+    IdCodigo    int(11),
+    Codigo      char(32)    not null,
 
-    primary key (idCodigo)
+    primary key (IdCodigo)
 );
 
 
     --   ASIGNATURA    --
 create table ASIGNATURA (
-    idAsignatura    int(11)         not null,
-    codigo          varchar(10)     not null,
-    nombre          varchar(100)    not null,
-    estudios        int(11)         not null,
-    curso           int(1)          not null,
+    IdAsignatura    int(11)         not null,
+    Codigo          varchar(6)     not null,
+    Nombre          varchar(100)    not null,
+    Estudios        int(11)         not null,
+    Curso           int(1)          not null,
 
-    primary key    (idAsignatura),
-    foreign key    (estudios)        references ESTUDIOS(idEstudios),
+    primary key    (IdAsignatura),
+    foreign key    (Estudios)        references ESTUDIOS(IdEstudios),
 
     unique (codigo)
 );
@@ -56,74 +57,75 @@ create table ASIGNATURA (
 
     --   USUARIO    --
 create table USUARIO (
-    idUsuario       int(11),
-    nombre          varchar(30)    not null,
-    apellido1       varchar(30)    not null,
-    apellido2       varchar(30),
-    password        varchar(34)    not null,
-    nick            varchar(20)    not null,
-    email           varchar(50)    not null,
-    puntuación      float,
-    estudios        int(11),
+    IdUsuario       int(11),
+    Nombre          varchar(30)    not null,
+    Apellido1       varchar(30)    not null,
+    Apellido2       varchar(30),
+    Password        varchar(32)    not null,
+    Nick            varchar(20)    not null,
+    Email           varchar(50)    not null,
+    Puntuacion      float, 			--- Ésta se calculará con un método 
+    Estudios        int(11),
 
-    unique (nick),
-    unique (nombre, apellido1, apellido2),
+    unique (Nick),
+   unique (Nombre, Apellido1, Apellido2),	 ---Puede haber dos usuarios con el mismo nombre y apellidos
 
-    primary key    (idUsuario),
-    foreign key    (estudios)   references  ESTUDIOS(idEstudios)
+    primary key    (IdUsuario),
+    foreign key    (Estudios)   references  ESTUDIOS(IdEstudios)
 );
 
 
     --   SESION    --
 create table SESION (
-    idSesion    int(11)    not null,
-    idUsuario   int(11)    not null,
+    IdSesion    varchar(32)    not null,
+    IdUsuario   int(11)    not null,
 
-    primary key    (idSesion),
-    foreign key    (idUsuario)  references  USUARIO(idUsuario)
+    primary key    (IdSesion),
+    foreign key    (IdUsuario)  references  USUARIO(IdUsuario)
 );
 
 
     --   DOCUMENTOS    --
 create table DOCUMENTOS (
-    idDocumento   int(11)       not null,
-    titulo        varchar(50)   not null,
-    usuario       int(11),
-    fechaSubida   datetime      default current_timestamp,
-    tipo          int(11)       not null,
-    anio          int(11)       not null,
-    documento     varchar(50)   not null,
-    hash          varchar(32)   not null,
-    asignatura    int(11)       not null,
-    comentario    varchar(300),
-    puntuacion    float,
+    IdDocumento   int(11)       not null,
+    Titulo        varchar(50)   not null,
+    Usuario       int(11)	not null,
+    FechaSubida   datetime      default current_timestamp,
+    Tipo          int(11)       not null,
+    Anio          int(11)       not null,
+    Documento     varchar(50)   not null,
+    Hash          varchar(32)   not null,
+    Asignatura    int(11)       not null,
+    Comentario    varchar(500),
+    Puntuacion    float,
 
-    unique (hash),
+    unique (Hash),
 
-    primary key   (idDocumento),
-    foreign key   (usuario)     references  USUARIO(idUsuario),
-    foreign key   (tipo)        references  TIPO(idTipo),
-    foreign key   (anio)        references  ANIO(idAnio),
-    foreign key   (asignatura)  references  ASIGNATURA(idAsignatura)
+    primary key   (IdDocumento),
+    foreign key   (Usuario)     references  USUARIO(IdUsuario),
+    foreign key   (Tipo)        references  TIPO(IdTipo),
+    foreign key   (Anio)        references  ANIO(IdAnio),
+    foreign key   (Asignatura)  references  ASIGNATURA(IdAsignatura)
 );
 
 
     --   COMENTARIOS    --
 create table COMENTARIOS (
-    idComentario    int(11),
-    idUsuario       int(11),
-    idDocumento     int(11),
-    puntuacion      float,
-    comentario      varchar(300),
-    fecha           date,
-    pseudonimo      char(12),
+    IdComentario    int(11),
+    IdUsuario       int(11),
+    IdDocumento     int(11),
+    Puntuacion      float,
+    Comentario      varchar(300),
+    Fecha           date,
+    Pseudonimo      char(12),
 
-    primary key    (idComentario),
-    foreign key    (idUsuario)      references  USUARIO(idUsuario),
-    foreign key    (idDocumento)    references  DOCUMENTOS(idDocumento),
+    primary key    (IdComentario),
+    foreign key    (IdUsuario)      references  USUARIO(IdUsuario),
+    foreign key    (IdDocumento)    references  DOCUMENTOS(IdDocumento),
 
-    constraint usr_apunt  unique (idUsuario, idDocumento),
-    constraint apunt_pseu unique (idDocumento , pseudonimo),
+    constraint usr_apunt  unique (IdUsuario, IdDocumento),
+    constraint apunt_pseu unique (IdDocumento , Pseudonimo)
+	---,
 
-    check (puntuacion < 5.0)
+    ---check (puntuacion < 5.0) No funciona en MySQL
 );
