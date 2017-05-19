b644cf315a6bf3b097de9b7155d57e5e65318f42
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-05-19 13:15:34 +0200

    Pelu usa github

--- DataBase.sql
+++ DataBase.sql
@@ -1,21 +1,32 @@
-create database if not exists Apuntes;
+if exists(select * from sys.databases where name='Apuntes')
+  drop database Apuntes
+go
+
+
+create database Apuntes;
+
+go
 
 use Apuntes;
 
 
+
     --    AÑO    --
 create table ANIO (
-    idAnio    int(11)   not null,
-    anio      char(9),
+    idAnio    int     not null,
+    anio      char(9) not null,
+
     unique (anio),
+
     primary key (idAnio)
 );
 
 
     --   ESTUDIOS    --
 create table ESTUDIOS (
-    idEstudios    int(11)        not null,
+    idEstudios    int            not null,
     nombre        varchar(12)    not null,
+    descripcion    varchar(40), 
 
     primary key (idEstudios)
 );
@@ -23,29 +34,25 @@ create table ESTUDIOS (
 
     --   TIPO    --
 create table TIPO (
-    idTipo    int(11)        not null,
+    idTipo    int            not null,
     nombre    varchar(12)    not null,
+
+    constraint chk_nombre CHECK (nombre in ('Apuntes', 'Apuntes Profesor', 'Examenes')),
 
     primary key (idTipo)
 );
 
 
-    --   INVITACION    --
-create table INVITACION (
-    idCodigo    int(11),
-    codigo      char(12)    not null,
-
-    primary key (idCodigo)
-);
-
-
     --   ASIGNATURA    --
 create table ASIGNATURA (
-    idAsignatura    int(11)         not null,
+    idAsignatura    int             not null,
     codigo          varchar(10)     not null,
     nombre          varchar(100)    not null,
-    estudios        int(11)         not null,
-    curso           int(1)          not null,
+    estudios        int             not null,
+    curso           int             not null,
+    descripcion     varchar(300),
+
+    constraint chk_curso CHECK (curso between 1 and 4),
 
     primary key    (idAsignatura),
     foreign key    (estudios)        references ESTUDIOS(idEstudios),
@@ -56,15 +63,19 @@ create table ASIGNATURA (
 
     --   USUARIO    --
 create table USUARIO (
-    idUsuario       int(11),
-    nombre          varchar(30)    not null,
-    apellido1       varchar(30)    not null,
-    apellido2       varchar(30),
-    password        varchar(34)    not null,
-    nick            varchar(20)    not null,
-    email           varchar(50)    not null,
-    puntuación      float,
-    estudios        int(11),
+    idUsuario       int                              not null,
+    nombre          varchar(30)                      not null,
+    apellido1       varchar(30)                      not null,
+    apellido2       varchar(30)                              ,
+    password        varchar(34)                      not null,
+    nick            varchar(20)                      not null,
+    email           varchar(50)                      not null,
+    estudios        int                              not null,
+    fechaAlta       datetime     default getdate()           ,
+    fechaBaja       datetime                                 ,
+
+    constraint chk_fechaSub_fechaBaja  CHECK (fechaBaja > fechaAlta),
+    constraint chk_email CHECK (email like '%@%'),
 
     unique (email),
     unique (nick),
@@ -76,8 +87,8 @@ create table USUARIO (
 
     --   SESION    --
 create table SESION (
-    idSesion    int(11)    not null,
-    idUsuario   int(11)    not null,
+    idSesion    int    not null,
+    idUsuario   int    not null,
 
     primary key    (idSesion),
     foreign key    (idUsuario)  references  USUARIO(idUsuario)
@@ -86,19 +97,19 @@ create table SESION (
 
     --   DOCUMENTOS    --
 create table DOCUMENTOS (
-    idDocumento   int(11)       not null,
-    titulo        varchar(50)   not null,
-    usuario       int(11),
-    fechaSubida   datetime      default current_timestamp,
-    tipo          int(11)       not null,
-    anio          int(11)       not null,
-    documento     varchar(50)   not null,
-    hash          varchar(32)   not null,
-    asignatura    int(11)       not null,
-    comentario    varchar(300),
-    puntuacion    float,
-    fechadescat   datetime      null,
+    idDocumento   int                                     not null,
+    titulo        varchar(50)                             not null,
+    usuario       int                                     not null,
+    fechaSubida   datetime     default current_timestamp          ,
+    tipo          int                                     not null,
+    anio          int                                     not null,
+    documento     varchar(50)                             not null,
+    hash          varchar(32)                             not null,
+    asignatura    int                                     not null,
+    comentario    varchar(300)                            not null,
+    fechadescat   datetime                                        ,
 
+    constraint chk_fechaSubida CHECK (fechaSubida < fechadescat),
 
     unique (hash),
 
@@ -112,16 +123,33 @@ create table DOCUMENTOS (
 
     --   COMENTARIOS    --
 create table COMENTARIOS (
-    idUsuario       int(11),
-    idDocumento     int(11),
-    puntuacion      float,
-    comentario      varchar(300),
-    fecha           date,
-    pseudonimo      char(12),
+    idUsuario       int                                      not null,
+    idDocumento     int                                      not null,
+    puntuacion      float                                    not null,
+    comentario      varchar(300)                             not null,
+    fecha           datetime      default current_timestamp          ,
+    pseudonimo      char(12)      default 'anonimo'          not null,
+
+    constraint chk_puntuacion CHECK (puntuacion between 0 and 5)
 
     primary key    (idUsuario, idDocumento),
     foreign key    (idUsuario)      references  USUARIO(idUsuario),
     foreign key    (idDocumento)    references  DOCUMENTOS(idDocumento),
 
     constraint apunt_pseu unique (idDocumento , pseudonimo)
+);
+
+
+    -- REPORTES --
+create table REPORTES (
+    idReportes    int                                      not null,
+    idDocumento   int                                      not null,
+    idUsuario     int                                      not null, -- Usuario que hace el reporte
+    fechaReporte  datetime      default current_timestamp          ,
+    comentario    varchar(300)                             not null,
+
+    primary key (idReportes),
+    foreign key (idDocumento)    references DOCUMENTOS(idDocumento),
+    foreign key (idUsuario)      references USUARIO(idUsuario)
+
 );
